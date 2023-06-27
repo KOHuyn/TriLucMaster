34a1116a514d38930bfce6b2a61599e56be14dde
KOHuyn <huyhipvp9x@gmail.com>
2023-06-27 15:24:17 +0700

    init base

---
+++ QRArt/app/src/main/java/com/apero/qrart/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.apero.qrart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.apero.qrart.ui.QRArtApp
+import com.apero.qrart.ui.theme.QRArtTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            QRArtTheme {
+//                val systemUiController = rememberSystemUiController()
+                QRArtApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QRArtTheme {
+        Greeting("Android")
+    }
+}
